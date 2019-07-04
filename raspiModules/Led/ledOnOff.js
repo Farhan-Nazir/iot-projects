b043fdc7d07c820ec9ae013d963719421a5c5757
Farhan Nazir <nazirf3440@gmail.com>
2019-07-04 21:08:47 +0200

    fixed socket error

--- raspiModules/Led/ledOnOff.js
+++ raspiModules/Led/ledOnOff.js
@@ -2,10 +2,8 @@ let Gpio = require("onoff").Gpio;
 let Led = new Gpio(4, "out");
 
 // 0 = off, 1 = on
-function ledOnOff(socket) {
-  socket.on("OnOff", state => {
-    Led.writeSync(state);
-  });
+function ledOnOff(state) {
+   return Led.writeSync(state);
 }
 
 module.exports = ledOnOff;
